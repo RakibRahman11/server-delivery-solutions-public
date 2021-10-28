f4aed51d946160464f7592015755c1fcb23a82e4
RakibRahman11 <rahmanrakib207@gmail.com>
2021-10-29 03:11:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpttu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
