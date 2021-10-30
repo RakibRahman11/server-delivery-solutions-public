86aeaa08874672c99caa8636765f3c5bd9ec67d0
RakibRahman11 <rahmanrakib207@gmail.com>
2021-10-31 01:41:08 +0600

    heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const express = require('express')
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
